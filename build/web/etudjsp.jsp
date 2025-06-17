b5b3c252f499adeb61f28a7c5485091e33b7c910
developpeur <saraleu846@gmail.com>
2025-06-17 10:29:50 +0100

    first commit

---
+++ build/web/etudjsp.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : etudjsp
+    Created on : 17 juin 2025, 09:23:29
+    Author     : cash express
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>la moyenne est ${m}</h1>
+    </body>
+</html>
---
+++ src/java/m/etud.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package m;
+
+/**
+ *
+ * @author cash express
+ */
+public class etud {
+    String nom;
+    float note1;
+    float note2;
+
+    public etud(String nom, int note1, int note2) {
+        this.nom = nom;
+        this.note1 = note1;
+        this.note2 = note2;
+    }
+  public float calcul (int note1,int note2)
+  {return(note1+note2)/2;}  
+
+}
